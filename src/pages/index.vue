25a87265e9946cd9c4791e7d4b8803ce5e72134e
agushiagon <agonagushi96@gmail.com>
2024-07-20 22:41:31 +0200

    removed unUsed components and added table with dummy data

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,111 @@
 <template>
-  <HelloWorld />
+  <v-container fluid>
+    <v-card class="pa-8">
+      <v-row>
+        <v-col>
+          <v-text-field
+            v-model="search"
+            hide-details
+            label="Search"
+            max-width="300"
+            prepend-inner-icon="mdi-magnify"
+            variant="outlined"
+          />
+        </v-col>
+        <v-spacer />
+        <v-col class="d-flex align-center justify-end">
+          <v-btn>
+            Add New Contact
+          </v-btn>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-data-table
+            class="elevation-1"
+            :headers="headers"
+            hide-default-footer
+            item-key="email"
+            :items="contacts"
+            :search="search"
+          >
+            <template #item="props">
+              <tr>
+                <td class="text-left">
+                  {{ props.item.firstName }}
+                </td>
+                <td class="text-left">
+                  {{ props.item.lastName }}
+                </td>
+                <td class="text-left">
+                  {{ props.item.email }}
+                </td>
+                <td class="text-left">
+                  {{ props.item.country }}
+                </td>
+              </tr>
+            </template>
+            <template #no-data>
+              <v-card
+                class="d-flex align-center py-8"
+                flat
+                tile
+              >
+                <v-icon
+                  class="pa-3"
+                  color="primary"
+                  size="35"
+                >
+                  mdi-information-outline
+                </v-icon>
+                <div class="ma-3">
+                  <v-card-text class="text-h6 text-left primary--text pa-0">
+                    No Data
+                  </v-card-text>
+                </div>
+              </v-card>
+            </template>
+            <template #loading>
+              <v-skeleton-loader
+                height="400px"
+                type="table-row-divider@9"
+              />
+            </template>
+          </v-data-table>
+        </v-col>
+      </v-row>
+    </v-card>
+
+  </v-container>
+
 </template>
 
-<script lang="ts" setup>
-  //
+<script setup lang="ts">
+  import { ref } from 'vue'
+
+  interface Contact {
+    firstName: string;
+    lastName: string;
+    email: string;
+    country: string;
+  }
+
+  const headers = [
+    { title: 'First Name', value: 'firstName' },
+    { title: 'Last Name', value: 'lastName' },
+    { title: 'Email', value: 'email' },
+    { title: 'Country', value: 'country' },
+  ]
+
+  const contacts = ref<Contact[]>([
+    { firstName: 'John', lastName: 'Doe', email: 'john.doe@example.com', country: 'USA' },
+    { firstName: 'Jane', lastName: 'Doe', email: 'jane.doe@example.com', country: 'Canada' },
+  ])
+
+  const search = ref<string>('')
+
 </script>
+
+<style scoped>
+
+</style>
